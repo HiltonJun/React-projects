63c20e84094d44907805c048daea4d392aa52e58
HiltonJun <hilton.honda@gmail.com>
2022-07-05 21:52:30 -0300

    first commit

---
+++ game-live/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import  logo  from '../../assets/img/game-live-logo.png';
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <img src={ logo } alt="logo da game-live" />
+        </div>
+        <div>
+            <a href=""></a>
+            <a href=""></a>
+        </div>
+        <div>
+            <h2></h2>
+            <img src="" alt="imagem de perfil do usuário" />
+            <img src="" alt="imagem de logout" />
+        </div>
+    </header>
+  )
+}
+
+export default Header
