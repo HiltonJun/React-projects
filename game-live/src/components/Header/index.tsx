eb3ca5f1155f7be15891b847eb5e0975459ac37c
HiltonJun <hilton.honda@gmail.com>
2022-08-02 22:02:09 -0300

    Home page navbar header CSS basico

--- game-live/src/components/Header/index.tsx
+++ game-live/src/components/Header/index.tsx
@@ -1,22 +1,22 @@
-import  logo  from '../../assets/img/game-live-logo.png';
+import logo from "../../assets/img/game-live-logo-white.png";
+import logout from "../../assets/icons/switch.png";
+import avatar from "../../assets/img/avatar-user-1.jpg";
+import "./style.css";
 
 const Header = () => {
   return (
     <header>
-        <div>
-            <img src={ logo } alt="logo da game-live" />
-        </div>
-        <div>
-            <a href=""></a>
-            <a href=""></a>
-        </div>
-        <div>
-            <h2></h2>
-            <img src="" alt="imagem de perfil do usuário" />
-            <img src="" alt="imagem de logout" />
-        </div>
+        <img className="nav-logo" src={logo} alt="logo da game-live" />
+      <div className="nav-btns">
+        <h2>Favoritos</h2>
+        <h2>Gêneros</h2>
+      </div>
+      <div className="nav-profile">
+        <img id="nav-avatar"src={avatar} alt="imagem de perfil do usuário" />
+        <img id="nav-logout" src={logout} alt="imagem de logout" />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
